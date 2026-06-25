3691ff992264078e554b84af0ab531c9a833e0ce
Ruby-Rubin <rubinkanna.s@gmail.com>
2026-06-25 11:56:36 +0530

    changed LLM model to llama 4 scout instruct

--- evaluator.py
+++ evaluator.py
@@ -54,7 +54,7 @@ Transcript:
 """
 
   response = client.chat.completions.create(
-    model="llama-3.3-70b-versatile",
+    model="meta-llama/llama-4-scout-17b-16e-instruct",
     messages=[
         {
             "role": "user",
